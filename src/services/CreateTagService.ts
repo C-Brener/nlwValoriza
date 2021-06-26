e23defd090aaeb792752ec63e4351d8f8604b96f
brenermr <brenercaique0806@gmail.com>
2021-06-26 17:23:24 -0300

    Atualização 26/06

--- src/services/CreateTagService.ts
+++ src/services/CreateTagService.ts
@@ -13,7 +13,16 @@ class CreateTagService{
 
         const tagAlreadyExists = await tagsRepositories.findOne({
             name
-        })
+        });
+        if(tagAlreadyExists){
+            throw new Error("Tag Already Exists");
+        }
+        const tag = tagsRepositories.create({
+            name
+        });
+        await tagsRepositories.save(tag);
+        
+        return tag;
     }
 }
 
